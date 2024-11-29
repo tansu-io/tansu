628655dfed2d209bfd217ea4e098940294262fcc
Peter Morgan <peter.james.morgan@gmail.com>
2024-11-29 09:11:25 +0000

    cgd.e_tag, cgd.detail

--- tansu-storage/src/pg/consumer_group_detail.sql
+++ tansu-storage/src/pg/consumer_group_detail.sql
@@ -14,7 +14,7 @@
 -- You should have received a copy of the GNU Affero General Public License
 -- along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-select cg.e_tag, cg.detail
+select cgd.e_tag, cgd.detail
 
 from
 
